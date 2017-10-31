de4d45a1d57e2c55b6b87d6b1c045f6ed4c9def8
Michael Hruza <michael.hruza@gmail.com>
2017-10-31 19:45:08 +0000

    Added hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,7 +13,7 @@ class ContactsController < ApplicationController
  end
  
  private
- def contact_params
+  def contact_params
    params.require(:contact).permit(:name, :email, :comments)
- end
+  end
 end
